8da8dd4ab509f6b86386c8bbf95e1613a487fa6d
Sw3aty <72179940+Sw3aty-Acc@users.noreply.github.com>
2023-07-31 06:52:07 +0300

    Update example.py

--- example.py
+++ example.py
@@ -2,66 +2,80 @@
 This is a code example showing the main features of the library. 
 """
 
-from vishhhl import Menu, Option, Event, colorama, version
+from vishhhl.addons.Menu import *
+import colorama
+
 clrm = colorama.Fore
 
-login = None
+
+class Menu(mOption):
+    login = None
+
+    def __init__(self,
+                 title: str = None,
+                 desc: str = None,
+                 text: str = None,
+                 comment: str = None):
+        self.org_title = title
+        desc = desc if desc else f"This is the {colorama.Fore.MAGENTA}vishhhl{colorama.Style.RESET_ALL}!"
+        text = text if text else title
+        comment = comment if comment else desc
+
+        super().__init__(text, self, comment, title=title, desc=desc)
+        self.symbol = "\n"
+
+    def update(self):
+        if self.login:
+            self.changeTitle(f"{self.org_title} | Authorized: {self.login}")
+        else:
+            self.changeTitle(self.org_title)
+
 
 class clsSetting(Menu):
     def __init__(self):
-        super().__init__(f"Settings", desc=f"This is the {colorama.Fore.MAGENTA}vishhhl{colorama.Style.RESET_ALL} of version {version}!")
-        self.option = Option("Settings", obj_menu=self, desc="Customize button color")
-        self.addOption(Event("Red", func=self.changeColor, args=[0]),
-                       Event("Green", func=self.changeColor, args=[1]),
-                       Event("Cyan", func=self.changeColor, args=[2]),
-                       Event("Back", func=self.disable))
-        self.option_type = [r"\optText", r"\optColor > \optText < \optDesc \clrm.RESET"]
+        super().__init__("Settings",
+                         comment="Customize button color")
+        self.addOption(mLink("Red", link=self.changeColor, args=[0]),
+                       mLink("Green", link=self.changeColor, args=[1]),
+                       mLink("Cyan", link=self.changeColor, args=[2]),
+                       mLink("Back", link=self.disable))
 
     def changeColor(self, index):
-        for i in menMain.opt_list + self.opt_list:
+        for i in menMain.func_list + self.func_list:
             i.color = (clrm.RED, clrm.GREEN, clrm.CYAN)[index]
 
-    def update(self):
-        if login:
-            self.changeTitle(f"Settings | Authorized: {login}")
-        else:
-            self.changeTitle(f"Settings")
+    def on_selected(self):
+        ret = super().on_selected()
+        # input(ret)
+        return ret
+
 
 class clsMain(Menu):
     def __init__(self):
-        super().__init__("Menu", desc=f"This is the {colorama.Fore.MAGENTA}vishhhl{colorama.Style.RESET_ALL} of version {version}!")
-        self.optLogIn = Event(text="Log in", func=self.fLogIn, desc="Log in to continue")
-        self.optLogOut = Event(text="Log out", func=self.fLogOut, desc="Log out from system")
+        super().__init__("Menu")
+        self.optLogIn = mLink(text="Log in", link=self.fLogIn, comment="Log in to continue")
+        self.optLogOut = mLink(text="Log out", link=self.fLogOut, comment="Log out from system")
 
         menSetting = clsSetting()
-
-        eveInfo = Event(text="Information", func=input,
+        eveInfo = mLink(text="Information", link=input,
                         args=["\nEmail - sw3atyspace@gmail.com "
                               "\nYouTube - https://www.youtube.com/@sw3aty702 "
                               "\nDiscord Server - https://discord.gg/jchJKYqNmK"])
-        eveExit = Event(text="Exit", func=self.f_quit)
+        eveExit = mLink(text="Exit", link=self.fQuit)
 
-        self.addOption(self.optLogIn, menSetting.option, eveInfo, eveExit)
-
-    def update(self):
-        if login:
-            self.changeTitle(f"Menu | Authorized: {login}")
-        else:
-            self.changeTitle(f"Menu")
+        self.addOption(self.optLogIn, menSetting, eveInfo, eveExit)
 
     def fLogIn(self):
-        global login
-        login = input("\nEnter your name: ")
-        self.delOption(self.optLogIn)
-        self.addOption(self.optLogOut, index=0)
+        Menu.login = input("\nEnter your name: ")
+        self.delOptionByIndex(index=0)
+        self.addOptionByIndex(self.optLogOut, index=0)
 
     def fLogOut(self):
-        global login
-        login = None
-        self.delOption(self.optLogOut)
-        self.addOption(self.optLogIn, index=0)
+        Menu.login = None
+        self.delOptionByIndex(index=0)
+        self.addOptionByIndex(self.optLogIn, index=0)
 
-    def f_quit(self):
+    def fQuit(self):
         ans = input("\nDo you really want to go out (Y/n) ").upper()
         if ans == "Y":
             exit()
