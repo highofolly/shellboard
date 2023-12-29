fefdbb5674fae7eca887ed94227611ee524fb6d1
Sw3aty <72179940+Sw3aty-Acc@users.noreply.github.com>
2023-12-29 14:23:36 +0300

    Update __init__.py

--- vishhhl/__init__.py
+++ vishhhl/__init__.py
@@ -1,12 +1,12 @@
 """
-vishhhl version 0.3.2
+vishhhl version 0.3.3
 This library makes it easy to create a beautiful visual menu in the console. Based on the VisualMenu library.
 Email - sw3atyspace@gmail.com
 Discord Server - https://discord.com/invite/jchJKYqNmK
 Youtube - https://www.youtube.com/@sw3aty702
 """
 
-version = "0.3.2"
+version = "0.3.3"
 
 function = type(lambda: 0)
 
